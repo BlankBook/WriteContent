3ba193ee3b36cc3343c589aba82c50b684074b27
Jacob Brunsting <jacob.brunsting@gmail.com>
2017-06-07 12:34:04 -0400

    Add testing endpoint

--- server/api.go
+++ server/api.go
@@ -1,1 +1,17 @@
 package server
+
+import (
+    "fmt"
+    "net/http"
+
+    "github.com/blankbook/shared/web"
+)
+
+
+func SetupAPI(r *web.HttpRouter) {
+    r.HandleRoute(web.GET, "/test", getTest)
+}
+
+func getTest(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Testing!!")
+}
--- server/server.go
+++ server/server.go
@@ -1,1 +1,16 @@
 package server
+
+import (
+    "github.com/gorilla/mux"
+
+    "github.com/blankbook/shared/web"
+)
+
+const PATH_PREFIX = "/content/write"
+
+func SetupRoutes() {
+    muxRouter := mux.NewRouter()
+    r := web.NewHttpRouter(muxRouter, PATH_PREFIX)
+    SetupAPI(r)
+    r.StartListening();
+}
