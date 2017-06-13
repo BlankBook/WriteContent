f0680b18ca25f1a8dbd23770559cea9d0c7550dd
Jacob Brunsting <jacob.brunsting@gmail.com>
2017-06-13 14:18:08 -0400

    Add basic database connection logic

--- server/api.go
+++ server/api.go
@@ -1,18 +1,20 @@
 package server
 
 import (
+    "log"
     "net/http"
+    "database/sql"
 
     "github.com/blankbook/shared/models"
     "github.com/blankbook/shared/web"
 )
 
 // SetupAPI adds the API routes to the provided router
-func SetupAPI(r web.Router) {
-    r.HandleRoute([]string{web.POST}, "/post", postPost)
+func SetupAPI(r web.Router, db *sql.DB) {
+    r.HandleRoute([]string{web.POST}, "/post", PostPost, db)
 }
 
-func postPost(w http.ResponseWriter, queryParams map[string][]string, body string) {
+func PostPost(w http.ResponseWriter, queryParams map[string][]string, body string, db *sql.DB) {
     post, err := models.ParsePost(body)
     if err != nil {
             http.Error(w, err.Error(), http.StatusBadRequest)
@@ -23,6 +25,27 @@ func postPost(w http.ResponseWriter, queryParams map[string][]string, body strin
             http.Error(w, err.Error(), http.StatusBadRequest)
             return
     }
-    // send post to Cassandra database
+    err = db.Ping()
+    if err != nil {
+        log.Fatalf(err.Error())
+    }
+    rows, err := db.Query("SELECT Title FROM posts")
+    if err != nil {
+        log.Fatalf(err.Error())
+    }
+    var s string
+    defer rows.Close()
+    for rows.Next() {
+        err := rows.Scan(&s)
+        if err != nil {
+            log.Fatal(err)
+        }
+        log.Println(s)
+    }
+    err = rows.Err()
+    if err != nil {
+        log.Fatal(err)
+    }
+    // send post to database
     w.WriteHeader(http.StatusOK)
 }
--- server/server.go
+++ server/server.go
@@ -2,8 +2,12 @@ package server
 
 import (
     "os"
-
+    "fmt"
+    "log"
     "net/http"
+    "database/sql"
+
+    _ "github.com/denisenkom/go-mssqldb"
 
     "github.com/gorilla/mux"
 
@@ -11,15 +15,31 @@ import (
 )
 
 const pathPrefix = "/content/write"
+const databaseUsernameEnvVar = "BB_CONTENT_DB_USERNAME"
+const databasePasswordEnvVar = "BB_CONTENT_DB_PASSWORD"
+const databaseServerEnvVar = "BB_CONTENT_DB_SERVER"
+const databaseName = "blankbookcontent"
 
 // SetupRoutes configures the service API endpoints
 func SetupRoutes() {
-   
+    databaseUsername := os.Getenv(databaseUsernameEnvVar)
+    databasePassword := os.Getenv(databasePasswordEnvVar)
+    databaseServer := os.Getenv(databaseServerEnvVar)
+
+    db, err := sql.Open("mssql",
+                        fmt.Sprintf("sqlserver://%s:%s@%s?database=%s",
+                                    databaseUsername,
+                                    databasePassword,
+                                    databaseServer,
+                                    databaseName))
+    if err != nil {
+        log.Fatal(err)
+    }
 
     muxRouter := mux.NewRouter()
     muxRouter.NotFoundHandler = http.HandlerFunc(notFoundHandler)
     r := web.NewHTTPRouter(muxRouter, pathPrefix)
-    SetupAPI(r)
+    SetupAPI(r, db)
     r.StartListening()
 }
 
