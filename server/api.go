290e4ffb722b0f3e3d2451e3fd14980c7eecbf1f
Jacob Brunsting <jacob.brunsting@gmail.com>
2017-06-13 00:14:13 -0400

    Fix indentation

--- server/api.go
+++ server/api.go
@@ -1,28 +1,28 @@
 package server
 
 import (
-	"net/http"
+    "net/http"
 
-	"github.com/blankbook/shared/models"
-	"github.com/blankbook/shared/web"
+    "github.com/blankbook/shared/models"
+    "github.com/blankbook/shared/web"
 )
 
 // SetupAPI adds the API routes to the provided router
 func SetupAPI(r web.Router) {
-	r.HandleRoute([]string{web.POST}, "/post", postPost)
+    r.HandleRoute([]string{web.POST}, "/post", postPost)
 }
 
 func postPost(w http.ResponseWriter, queryParams map[string][]string, body string) {
-	post, err := models.ParsePost(body)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-	err = post.Validate()
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-	// send post to Cassandra database
-	w.WriteHeader(http.StatusOK)
+    post, err := models.ParsePost(body)
+    if err != nil {
+            http.Error(w, err.Error(), http.StatusBadRequest)
+            return
+    }
+    err = post.Validate()
+    if err != nil {
+            http.Error(w, err.Error(), http.StatusBadRequest)
+            return
+    }
+    // send post to Cassandra database
+    w.WriteHeader(http.StatusOK)
 }
--- server/server.go
+++ server/server.go
@@ -1,24 +1,28 @@
 package server
 
 import (
-	"net/http"
+    "os"
 
-	"github.com/gorilla/mux"
+    "net/http"
 
-	"github.com/blankbook/shared/web"
+    "github.com/gorilla/mux"
+
+    "github.com/blankbook/shared/web"
 )
 
 const pathPrefix = "/content/write"
 
 // SetupRoutes configures the service API endpoints
 func SetupRoutes() {
-	muxRouter := mux.NewRouter()
-	muxRouter.NotFoundHandler = http.HandlerFunc(notFoundHandler)
-	r := web.NewHTTPRouter(muxRouter, pathPrefix)
-	SetupAPI(r)
-	r.StartListening()
+   
+
+    muxRouter := mux.NewRouter()
+    muxRouter.NotFoundHandler = http.HandlerFunc(notFoundHandler)
+    r := web.NewHTTPRouter(muxRouter, pathPrefix)
+    SetupAPI(r)
+    r.StartListening()
 }
 
 func notFoundHandler(w http.ResponseWriter, r *http.Request) {
-	http.Error(w, "endpoint not found", http.StatusNotFound)
+    http.Error(w, "endpoint not found", http.StatusNotFound)
 }
